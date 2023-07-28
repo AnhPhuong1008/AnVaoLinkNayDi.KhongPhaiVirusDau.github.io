210fa14f0b6217543f0660a4a0f53b54ea59d451
AnhPhuong1008 <anhphuongnt1008@gmail.com>
2023-07-28 15:55:01 +0700

    virus

---
+++ core.js
@@ -0,0 +1,7 @@
+function startHacking() {
+    document.getElementById('hacking-section').style.display = 'block';
+}
+
+function stopHacking() {
+    document.getElementById('hacking-section').style.display = 'none';
+}
